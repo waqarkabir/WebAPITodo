87a6b96c2266838b6da997017e97dd88a3ec1493
waqarkabir <waqarkabir10@gmail.com>
2024-09-20 09:07:56 +0500

    Endpoint Filters => Validation Filter

--- Program.cs
+++ Program.cs
@@ -53,6 +53,26 @@ namespace WebAPI
             {
                 todos.Add(task);
                 return TypedResults.Created("/todos/{id}", task);
+            })
+            .AddEndpointFilter(async (context, next) =>
+            {
+                var taskArgument = context.GetArgument<Todo>(0);
+                var errors = new Dictionary<string, string[]>();
+                if(taskArgument.DueDate < DateTime.UtcNow)
+                {
+                    errors.Add(nameof(Todo.DueDate) , ["Cannot have due date in the past."]);
+                }
+                if(taskArgument.IsCompleted)
+                {
+                    errors.Add(nameof(Todo.IsCompleted) , ["Cannot add completed todo."]);
+                }
+
+                if (errors.Count > 0)
+                {
+                    return Results.ValidationProblem(errors);                    
+                }
+
+                return await next(context);
             });
 
             app.MapDelete("/todos/{id}", (int id) =>
